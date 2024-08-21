d4acf2fee4a73d4ad928597a40356ed479796573
ViniciusNevesSalamene <150290843+ViniciusNevesSalamene@users.noreply.github.com>
2024-08-21 10:49:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function showtime(){
     setTimeout(()=>{
         var agora = new Date();
         var dia = new Date('2024-08-17')
-        var diferenca = Math.abs((agora.getTime()-10800000) - dia.getTime())
+        var diferenca = Math.abs((agora.getTime()-(10800000+46800000)) - dia.getTime())
         var dias = Math.floor(diferenca / (1000*60*60*24))
         var horas = Math.floor((diferenca - (dias*1000*60*60*24)) / (1000*60*60))
         var minutos = Math.floor((diferenca - ((dias*1000*60*60*24)+(horas*1000*60*60))) / (1000*60))
